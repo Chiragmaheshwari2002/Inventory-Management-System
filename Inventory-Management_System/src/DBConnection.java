b4ab48d0fe203e24efc724563590892d2592ec53
Chirag Maheshwari <156605236+Chiragmaheshwari2002@users.noreply.github.com>
2025-04-05 19:22:17 +0530

    Update DBConnection.java
    add Drivers control statements and update the string

--- Inventory-Management_System/src/DBConnection.java
+++ Inventory-Management_System/src/DBConnection.java
@@ -2,12 +2,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/?user=root";
-    private static final String USER = "root"; // change if different
-    private static final String PASSWORD = "chirag2002"; // change if different
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/<DATABASENAME>";
+    private static final String USER = "<USERNAME>"; // change if different
+    private static final String PASSWORD = "<PASSWORD>"; // change if different
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());;
+        }
     }
 }
